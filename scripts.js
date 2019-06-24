686a0f0151f25e876bc582d65f475194bd607139
Dana L. Brown <dana.l.brown@gmail.com>
2019-06-23 20:26:27 -0400

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,1 +1,16 @@
+console.log("Hello World");
 
+var canvas = document.getElementById("canvas");
+var c= canvas.getContext('2d");
+var tx = window.innerWidth;
+var ty = window.innerHeight;     
+canvas.width = tx;
+canvas.height = ty;
+//c.linewidth= 5;
+//c.globalAlpha = o.5;
+
+var mousex = 0;
+var mousey = 0;
+                         
+                         
+                         
